24fccdaf58863ef37872cfe9ebf9e72985a79145
levuhieu151 <hieulvhe160841@fpt.edu.vn>
2022-12-16 14:24:57 +0700

    OTP Timer With Animation

--- src/components/OTP/OTP.js
+++ src/components/OTP/OTP.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import CountDownAnimation from "./CountDownAnimation";
 import GenarateOTP from "./GenerateOTP";
 import InputOTP from "./InputOTP";
 import './OTP.scss';
